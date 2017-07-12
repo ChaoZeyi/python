084fb93cf2f678b36d8ac246fdd29939ab1f2f99
ChaoZeyi <chaozeyi1995@sina.com>
2017-07-12 16:55:54 +0800

    "test"

--- packages/sklearn_ex/codes/ex02.py
+++ packages/sklearn_ex/codes/ex02.py
@@ -4,7 +4,9 @@ import pickle
 clf =  svm.SVC(gamma=0.001, C=100)
 iris = datasets.load_iris()
 iris
+###############手写数字识别系统#########################
 digits = datasets.load_digits()
+digits
 x = digits.data
 y = digits.target
 clf.fit(x[:-1], y[:-1])
